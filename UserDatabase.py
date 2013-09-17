aa125263123d451b3954bb65dea16c4500af37fd
Ben Hirsch <BenHirsch@lawn-128-61-57-53.lawn.gatech.edu>
2013-09-17 14:03:49 -0400

    CSS and backend

--- UserDatabase.py
+++ UserDatabase.py
@@ -3,24 +3,17 @@ from Handler import *
 class Kid(ndb.Model):
 	first_name = ndb.StringProperty(required=True)
 	last_name = ndb.StringProperty(required=True)
-	attending_service = ndb.StringProperty(required=True)
 	attending_party = ndb.StringProperty(required=True)
-	shirt_size=ndb.StringProperty(required=True)
 
 class Adult(ndb.Model):
 	first_name = ndb.StringProperty(required=True)
 	last_name = ndb.StringProperty(required=True)
-	attending_service = ndb.StringProperty(required=True)
-	attending_party = ndb.StringProperty(required=True)
+	attending_luncheon = ndb.StringProperty(required=True)
 
 def addKid(fname, lname, attendingp):
-	kid = Kid(first_name=fname, last_name=lname, attending_service=attendings, attending_party=attendingp, shirt_size=shirt)
+	kid = Kid(first_name=fname, last_name=lname, attending_party=attendingp)
 	kid.put()
-	user = User(first_name=fname, last_name=lname, attending_service=attendings, attending_party=attendingp)
-	user.put()
 
 def addAdult(fname, lname, attendingl):
-	intown = Intown(first_name=fname, last_name=lname, attending_service=attendings, attending_party=attendingp)
-	intown.put()
-	user = User(first_name=fname, last_name=lname, attending_service=attendings, attending_party=attendingp)
-	user.put()
+	adult = Adult(first_name=fname, last_name=lname, attending_luncheon=attendingl)
+	adult.put()
